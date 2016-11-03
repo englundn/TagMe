44dca931d2ee2005464d10f8dcab77919bd9ab57
Andrew <anxsaiyuki@gmail.com>
2016-11-03 09:52:13 -0700

    Add tags and new style for search

--- client/memories.js
+++ client/memories.js
@@ -39,7 +39,15 @@ export default class Memories extends React.Component {
 
     });
     this.setState({ fontLoaded: true });
-    this.fetchMemories();
+
+    if (this.props.tag !== null && this.props.prevScene === 'Memory') {
+      this.setState({
+        searchTerm: this.props.tag
+      })
+      this.search();
+    } else {
+      this.fetchMemories();
+    }
   }
 
   _navigate(image) {
@@ -184,7 +192,7 @@ export default class Memories extends React.Component {
           <ScrollView tabLabel='Photos'>
             <Content contentContainerStyle={{
               flexWrap: 'wrap',
-              flexDirection: 'row',
+              flexDirection: 'column',
               justifyContent: 'center',
               alignItems: 'center'
             }}>
@@ -208,13 +216,11 @@ export default class Memories extends React.Component {
                 ) : null
               }
             </View>
-            <View style={{flexDirection:'column'}}>
-              <View>
-                <View style={styles.tagsContainer}>
-                  {searchQueueNode}
-                </View>
-              </View>
-              <View style={{flexDirection: 'row'}}>
+            <View style={styles.tagsContainer}>
+              {searchQueueNode}
+            </View> 
+            <View style={{flexDirection: 'row', margin: 3}}> 
+            <View style={{flex: 1, flexDirection: 'row', flexWrap: 'wrap', alignItems: 'flex-start'}}>
                 {
                   this.state.searching ? (
                     this.state.queryList.map((image, i) => 
@@ -230,12 +236,17 @@ export default class Memories extends React.Component {
                     </TouchableHighlight>
                   )
                 }
-              </View>
             </View>
+            </View> 
             </Content>
           </ScrollView>
 
-          <ScrollView tabLabel='Tags'></ScrollView>
+          <ScrollView tabLabel='Tags'>
+            <View style={styles.tagsContainer}>
+                {searchQueueNode}
+            </View>
+
+          </ScrollView>
 
         </ScrollableTabView>
       </View>
@@ -268,12 +279,14 @@ const styles = StyleSheet.create({
   },
 
   tagsContainer: {
+    marginTop: 30,
+    marginBottom: 10,
     flexWrap: 'wrap',
     flexDirection: 'row',
     justifyContent: 'center',
     alignItems: 'center',
-    margin: 10,
-  }
+    flex: 1
+  },
 
 });
 
